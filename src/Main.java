c991d2983ae531bb1ffab3095674c4b97fac1889
mitodz <mitodzzz@gmail.com>
2019-05-06 20:45:37 +0300

    Implementation process

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner("5 5\n" +
+                "1 1 1 1 1\n" +
+                "3 5\n" +
+                "2 4\n" +
+                "1 4\n" +
+                "5 4\n" +
+                "5 3");
+        int n = scanner.nextInt(); //количество таблиц
+        int m = scanner.nextInt(); //количество запросов
+        Table t = new Table();
+        for (int i = 0; i < n; i++) {
+           t.makeSet(scanner.nextInt());
+        }
+        System.out.println();
     }
 }
--- src/Table.java
+++ src/Table.java
@@ -1,8 +1,12 @@
-public class DisjointUnion {
-    private int[] parent = new int[Integer.MAX_VALUE];
-    private int[] rank = new int[Integer.MAX_VALUE];
+public class Table {
+    private int[] parent = new int[100000];
+    private int[] rank = new int[100000];
+    private int maxTable=0;
 
     public void makeSet (int i) {
+        if (i > maxTable) {
+            maxTable = i;
+        }
         parent[i] = i;
         rank[i] = 0;
     }
@@ -14,17 +18,24 @@ public class DisjointUnion {
         return parent[i];
     }
 
+    public int getMaxTable() {
+        return maxTable;
+    }
+
     public void union (int i, int j) {
         int i_id = find(i);
         int j_id = find(j);
         if (i_id==j_id) return;
         if (rank[i_id] > rank[j_id]) {
             parent[j_id] = i_id;
+            maxTable = Integer.max(rank[i_id], maxTable);
         } else {
             parent[i_id] = j_id;
             if (rank[i_id] == rank[j_id]) {
                 rank[j_id] += 1;
+                maxTable = Integer.max(rank[j_id], maxTable);
             }
         }
+
     }
 }
